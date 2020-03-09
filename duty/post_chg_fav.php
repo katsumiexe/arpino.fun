d4928c2ea4706daf8911059f7aa352b27ee0a2bc
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-03-09 17:16:46 +0900

    ar

--- duty/post_chg_fav.php
+++ duty/post_chg_fav.php
@@ -1,5 +1,24 @@
 <?
-$mysqli = mysqli_connect("localhost", "tiltowait_model", "kk1941", "tiltowait_model");
+//■■サーバー接続----------------------*
+$ip_ok=1;
+if ($dir = opendir("config/")) {
+	while (($file = readdir($dir)) !== false) {
+		if ($file != "." && $file != ".." ) {
+			$fp=fopen("./config/{$file}","r");
+			if($fp){
+				$set_title=trim(fgets($fp));
+				$set_host=trim(fgets($fp));
+				$set_user=trim(fgets($fp));
+				$set_pass=trim(fgets($fp));
+				$set_data=trim(fgets($fp));
+				$set_key=str_replace('kk','',$file);
+				$set_key=str_replace('.txt','',$set_key);
+			}
+		}
+	} 
+closedir($dir);
+}
+$mysqli = mysqli_connect($set_host, $set_user, $set_pass, $set_data);
 if(!$mysqli){
 	$msg="接続エラー";
 	die("接続エラー");
@@ -7,24 +26,25 @@ if(!$mysqli){
 
 mysqli_set_charset($mysqli,'UTF-8'); 
 
-
 $chg	=$_POST["chg"];
 $uid	=$_POST["uid"];
 $log_id	=$_POST["log_id"];
 
 
+$sql ="SELECT * FROM duty_list"; 
+$sql.=" WHERE user='{$uid}'";
+$sql.=" AND fav_log='{$log_id}'";
 
-$sql2 ="UPDATE me_fav SET"; 
-$sql2.=" WHERE user_id='{$host_id}'";
-$sql2.=" GROUP BY day,action";
-$sql2.=" ORDER BY log_id DESC";
+if (mysqli_query($mysqli,$sql)){
+	$sql1 ="UPDATE duty_list SET"; 
+	$sql1.=" fav_id='{$chg}'";
+	$sql1.=" WHERE user='{$uid}'";
+	$sql1.=" AND fav_log='{$log_id}'";
+}else{
+	$sql1 ="INSERT INTO duty_list(fav_id,fav_log,user)VALUES('{chg}','{log_id}','{uid}')"; 
+}
+mysqli_query($mysqli,$sql1);
 
-
-
-echo json_encode($dat_p);
 exit;
 ?>
-
-
-
 
