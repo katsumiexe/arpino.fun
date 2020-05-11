afc11adedc9fa9a58f7f7d33cd967154dc15a8f3
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-05-11 12:55:39 +0900

    ar

--- sample20200508/cast.php
+++ sample20200508/cast.php
@@ -2,7 +2,6 @@
 include_once("./library/session.php");
 
 
-
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -13,24 +12,6 @@ include_once("./library/session.php");
 <script src="../onlyme/js/jquery-3.2.1.min.js"></script>
 <script>
 $(function(){ 
-var AllCnt=<?=count($list)-1?>;
-var NowCnt=<?=count($list)-1?>;
-	$('.tag_a').on('click', function() {
-		if($(this).hasClass("tag_b")){
-			$(this).removeClass("tag_b");
-			NowCnt--;
-		}else{
-			$(this).addClass("tag_b");
-			NowCnt++;
-		}		
-
-		$('.box').hide();
-		Tmp = $(this).attr('id');
-		$('.'+Tmp).fadeIn();
-
-		$('#max_cnt').text(AllCnt);
-		$('#now_cnt').text(NowCnt);
-	});
 });
 </script>
 <style>
@@ -38,7 +19,18 @@ var NowCnt=<?=count($list)-1?>;
 	background:#f0f0f0;
 	text-align:center;
 }
+
+.menu{
+	display			:inline-block;
+	background		:#ffd0d0;
+	width			:300px;
+	height			:400px;
+	padding			:20px;
+	border-radius	:20px;
+}
+
 .main{
+	display			:inline-block;
 	background		:#fafafa;
 	width			:100vw;
 	height			:100vh;
@@ -48,11 +40,12 @@ var NowCnt=<?=count($list)-1?>;
 
 .login_box{
 	display			:inline-block;
-	width			:80%;
+	width			:70%;
 	border			:1vw solid #ffd0d0;
-	text-shadow		:1vw 1vw 1vw rgba()30,30,30,0.6);
+	box-shadow		:1vw 1vw 1vw rgba(30,30,30,0.6);
 	border-radius	:2vw;
 	margin			:10vh auto;
+	padding			:1vw;
 	background		:#fff0f5
 }
 
@@ -81,10 +74,24 @@ var NowCnt=<?=count($list)-1?>;
 	height			:8vw;
 }
 
+.menu_1{
+	display		:block;
+	margin		:0 auto;
+	width		:260px;
+	height		:50px;
+	line-height	:50px;
+	text-align	:left;
+	font-size	:16px;
+	border		:2px solid #fafafa;
+	background	:#f1c0d0;
+	padding-left:10px;
+	color		:#fafafa;
+	font-weight	:700;
+}
 </style>
+<?$_SESSION["id"]=1;?>
 <body class="body">
 <?if(!$_SESSION["id"]){?>
-	<div class="menu"></div>
 	<div class="main">
 	<div class="login_box">
 		<span class="login_name">IDCODE</span>
@@ -95,7 +102,15 @@ var NowCnt=<?=count($list)-1?>;
 	</div>
 	</div>
 <?}else{?>
-	<div class="menu"></div>
+	<div class="menu">
+	<span id="m1" class="menu_1">TOP</span>
+	<span id="m2" class="menu_1">シフト</span>
+	<span id="m3" class="menu_1">写真</span>
+	<span id="m4" class="menu_1">日記</span>
+	<span id="m5" class="menu_1">BBS</span>
+	<span id="m6" class="menu_1">TL</span>
+	<span id="m7" class="menu_1">MAIL</span>
+	</div>
 	<div class="main"></div>
 <?}?>
 </div>
