0f2a316a53075ca5cc295576772ded8c3c079f99
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-05-13 11:49:11 +0900

    ar

--- mail_test.php
+++ mail_test.php
@@ -1,16 +1,21 @@
 <?
 $sv="{m15.coreserver.jp:110/pop3}INBOX";
-$sv="{m15.coreserver.jp:110}INBOX";
+$sv="{m15.coreserver.jp}INBOX";
 $m_list=imap_open ($sv,'info@onlyme.fun','katsumi1941');
 if(!$m_list){
 	print("error");
 }else{
 	$list = imap_list($m_list, $sv, "*");
+
 	foreach ($list as $a1 =>$a2) {
-		echo $a1."●".imap_utf7_decode($a2) . "\n";
+		echo $a1."●". $a2."●".imap_utf7_decode($a2) . "<br>\n";
+echo "<hr>";
+var_dump($a2;)
 	}
+
+//var_dump($list);
 }
 ?>
 <hr>
-<?=time()?>■<?=$minfo?>
+
 
