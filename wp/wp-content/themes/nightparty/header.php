ddde68c1cda7e952dd26c001072bff018ce1316c
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-05-14 12:40:25 +0900

    ar

--- wp/wp-content/themes/nightparty/header.php
+++ wp/wp-content/themes/nightparty/header.php
@@ -10,6 +10,7 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?t=<?=time()?>">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/person.css?t=<?=time()?>">
 <?php wp_head(); ?>
+<?php include(get_template_directory_uri()."/libraly/inc_cast.php"); ?>
 </head>
 <body class="body">
 <div class="head">
---
+++ wp/wp-content/themes/nightparty/library/inc_db.php
@@ -0,0 +1,8 @@
+<?php
+$tables[]='0cast';
+$tables[]='0castmail';
+$tables[]='0charm_table';
+$tables[]='0schedule';
+$tables[]='0staff';
+?>
+
