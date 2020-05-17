d714ff5a3ca81d439f738f45ba081c5447375a0b
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-05-17 11:35:29 +0900

    ar

--- wp/wp-content/themes/nightparty/page-person.php
+++ wp/wp-content/themes/nightparty/page-person.php
@@ -5,16 +5,17 @@ $val="1";
 $res = $wpdb->get_results('
 	SELECT * FROM wp01_0cast WHERE id="{$val}"
 ');
+foreach($res as $a1);
 
-if($res->face1 > 0):
-	$res->face=get_template_directory_uri()."/img/cast/".$res->id."/".$res->face1.".jpg";			
+if($a1->face1 > 0):
+	$a1->face=get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face1.".jpg";			
 else:
-	$res->face=get_template_directory_uri()."/img/cast/noimage.jpg";			
+	$a1->face=get_template_directory_uri()."/img/cast/noimage.jpg";			
 endif;
 ?>
 <div class="main">
 <div class="person_left">
-	<img src="<?PHP ECHO $res->face?>" class="person_img_main">
+	<img src="<?PHP ECHO $a1->face?>" class="person_img_main">
 	<div class="person_img_list">
 		<img id="i1" src="" class="person_img_sub">
 		<img id="i2" src="" class="person_img_sub">
@@ -23,7 +24,7 @@ endif;
 	</div>
 </div>
 <div class="person_right">
-	<?PHP ECHO $res->genji?>
+	<?PHP ECHO $a1->genji?>
 </div>
 
 </div>
