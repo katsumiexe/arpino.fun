6c4eee9761928023382faad7cc1dbfe5b252ca24
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-05-17 12:25:59 +0900

    ar

--- wp/wp-content/themes/nightparty/js/main.js
+++ wp/wp-content/themes/nightparty/js/main.js
@@ -4,4 +4,12 @@ $(function(){
 		$('#val_p').val(TMP);
 		$('#form_p').submit();
 	});
+
+	$('.person_img_sub').hover(function () {
+		TMP=$(this).attr('src');
+		$('.person_img_main').attr('src',TMP);
+
+	});
+
+
 });
--- wp/wp-content/themes/nightparty/page-person.php
+++ wp/wp-content/themes/nightparty/page-person.php
@@ -1,26 +1,40 @@
 <?php
 get_header();
 $val="1";
+$sql="SELECT * FROM wp01_0cast WHERE id='".$val."'";
 
-$res = $wpdb->get_results('
-	SELECT * FROM wp01_0cast WHERE id="{$val}"
-');
+$res = $wpdb->get_results($sql);
 foreach($res as $a1);
 
 if($a1->face1 > 0):
-	$a1->face=get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face1.".jpg";			
+	$face_a="<img src=\"".get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face1.".jpg\" class=\"person_img_main\">";
+	$face_b="<img id=\"i1\" src=\"".get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face1.".jpg\" class=\"person_img_sub\">";
 else:
 	$a1->face=get_template_directory_uri()."/img/cast/noimage.jpg";			
+	$face_a="<img src=\"".get_template_directory_uri()."/img/cast/noimage.jpg\" class=\"person_img_main\">";
+
 endif;
+
+if($a1->face2 > 0):
+	$face_b.="<img id=\"i2\" src=\"".get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face2.".jpg\" class=\"person_img_sub\">";
+endif;
+
+if($a1->face3 > 0):
+	$face_b.="<img id=\"i3\" src=\"".get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face3.".jpg\" class=\"person_img_sub\">";
+endif;
+
+if($a1->face4 > 0):
+	$face_b.="<img id=\"i4\" src=\"".get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face4.".jpg\" class=\"person_img_sub\">";
+endif;
+
+
+
 ?>
 <div class="main">
 <div class="person_left">
-	<img src="<?PHP ECHO $a1->face?>" class="person_img_main">
+		<?PHP ECHO $face_a?>
 	<div class="person_img_list">
-		<img id="i1" src="" class="person_img_sub">
-		<img id="i2" src="" class="person_img_sub">
-		<img id="i3" src="" class="person_img_sub">
-		<img id="i4" src="" class="person_img_sub">
+		<?PHP ECHO $face_b?>
 	</div>
 </div>
 <div class="person_right">
