2c7a9e3b5c963f5063b8859ee3c8b301463f155a
katsumiexe <counterpost2016@gmail.com>
2020-05-18 19:35:12 +0900

    ar

--- wp/wp-content/themes/nightparty/page-person.php
+++ wp/wp-content/themes/nightparty/page-person.php
@@ -1,5 +1,13 @@
 <?php
 get_header();
+$week[0]="(日)";
+$week[1]="(月)";
+$week[2]="(火)";
+$week[3]="(水)";
+$week[4]="(木)";
+$week[5]="(金)";
+$week[6]="(土)";
+
 $val="1";
 $sql="SELECT * FROM wp01_0cast WHERE id='".$val."'";
 
@@ -27,6 +35,7 @@ if($a1->face4 > 0):
 	$face_b.="<img id=\"i4\" src=\"".get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face4.".jpg\" class=\"person_img_sub\">";
 endif;
 ?>
+<div class="person_main">
 
 <div class="person_left">
 		<?PHP ECHO $face_a?>
@@ -34,11 +43,26 @@ endif;
 		<?PHP ECHO $face_b?>
 	</div>
 </div>
+
 <div class="person_middle">
+<table>
+<tr>
+<td class="parson_td_1">名前</td>
+<td class="parson_td_2"><?PHP ECHO $a1->genji?></td>
+</tr>
+</table>
+
+<table>
+<?PHP for($n=0;$n<7;$n++){?>
+<tr>
+<td class="parson_sch_1"><?PHP ECHO date("m/d",time()+(86400*$n))?><?PHP ECHO $week[date("w",time()+(86400*$n))]?></td>
+<td class="parson_sch_2"></td>
+</tr>
+<?}?>
+</table>
 
 </div>
 <div class="person_right">
-	<?PHP ECHO $a1->genji?>
 </div>
-
+</div>
 <?php get_footer(); ?>
