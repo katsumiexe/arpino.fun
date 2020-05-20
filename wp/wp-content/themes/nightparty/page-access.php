a6eed572515d51ec36c5e51ad038f83a32850e17
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-05-20 09:46:38 +0900

    ar

--- wp/wp-content/themes/nightparty/page-access.php
+++ wp/wp-content/themes/nightparty/page-access.php
@@ -1,24 +1,9 @@
-<?php get_header(); ?>
-任意のHTMLコードを入力しページをデザイン可能です。
-例えば、画像を表示する。スライドショーを表示するなど、任意のHTMLコードを使えます。
-<div id="container">
- 
-<?php if(have_posts()): while(have_posts()): the_post(); ?>
-<?php the_content(); ?>
-<?php endwhile; endif; ?>
- 
-</div><!-- [ /#container ] -->
- 
-<div id="news-box" style="margin-top:100px; color:#fafafa;font-size:16px;">
-<ul>
 <?php
-$myposts = get_posts('post_type=post&numberposts=5&category='.$var);
-foreach($myposts as $post) : ?>
- 
-<li><?php the_time('Y年m月d日(D)'); ?><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
-<?php endforeach; ?>
- 
-</ul>
-</div><!-- [ /#news-box ] -->
- 
+get_header();
+$pg = get_post(72);
+echo $pg->post_content;
+
+?>
+
+
 <?php get_footer(); ?>
--- wp/wp-content/themes/nightparty/page-cast.php
+++ wp/wp-content/themes/nightparty/page-cast.php
@@ -8,7 +8,7 @@ $res = $wpdb->get_results('
 ');
 $n=0;
 foreach($res as $a1):
-	$dat[$n]		=$a1;
+	$dat[$n]=$a1;
 	if($a1->face1 > 0):
 		$dat[$n]->face=get_template_directory_uri()."/img/cast/".$a1->id."/".$a1->face1.".jpg";			
 	else:
@@ -21,8 +21,8 @@ wp_reset_postdata();
 <div class="main">
 	<div class="main_b_all">
 		<? for($s=0;$s<$n;$s++){?>
-			<a href="<?php echo get_template_directory_uri(); ?>/person/" id="<?PHP echo $dat[$s]->id?>" class="main_b_1">
-			<img src="<?PHP echo $dat[$s]->face?>" class="main_b_1_1">
+			<a href="<?PHP ECHO get_template_directory_uri(); ?>/person/" id="<?PHP echo $dat[$s]->id?>" class="main_b_1">
+			<img src="<?PHP ECHO $dat[$s]->face?>" class="main_b_1_1">
 			<span class="main_b_1_2"><?PHP echo $dat[$s]->genji?></span>
 			<div class="main_b_1_3"></div>
 			</a>
