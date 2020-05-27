64eb5c715d8db40361ea9b994716616ec3afbaea
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-05-27 16:29:01 +0900

    ar

--- wp/wp-content/themes/nightparty/page-access.php
+++ wp/wp-content/themes/nightparty/page-access.php
@@ -1,11 +1,9 @@
 <?php
 get_header();
 ?>
-<div class="main">
-	<div class="main_b_all">
-		<div class="access_top">
+<div class="main_b_all">
+	<div class="access_top">
 
-		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
--- wp/wp-content/themes/nightparty/page-cast.php
+++ wp/wp-content/themes/nightparty/page-cast.php
@@ -18,15 +18,13 @@ foreach($res as $a1):
 endforeach;
 wp_reset_postdata();
 ?>
-<div class="main">
-	<div class="main_b_all">
-		<? for($s=0;$s<$n;$s++){?>
-			<a href="<?PHP ECHO get_template_directory_uri(); ?>/person/<?PHP echo $dat[$s]->id?>" id="<?PHP echo $dat[$s]->id?>" class="main_b_1">
-			<img src="<?PHP ECHO $dat[$s]->face?>" class="main_b_1_1">
-			<span class="main_b_1_2"><?PHP echo $dat[$s]->genji?></span>
-			<div class="main_b_1_3"></div>
-			</a>
-		<? } ?>
-	</div>
+<div class="main_b_all">
+	<? for($s=0;$s<$n;$s++){?>
+		<a href="<?PHP ECHO get_template_directory_uri(); ?>/person/<?PHP echo $dat[$s]->id?>" id="<?PHP echo $dat[$s]->id?>" class="main_b_1">
+		<img src="<?PHP ECHO $dat[$s]->face?>" class="main_b_1_1">
+		<span class="main_b_1_2"><?PHP echo $dat[$s]->genji?></span>
+		<div class="main_b_1_3"></div>
+		</a>
+	<? } ?>
 </div>
 <?php get_footer(); ?>
