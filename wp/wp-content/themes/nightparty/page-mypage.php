ab8e7d8523f5e0bcce4a0aa68a6bc3b489f42bdc
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-06-06 08:25:11 +0900

    ar

--- wp/wp-content/themes/nightparty/page-mypage.php
+++ wp/wp-content/themes/nightparty/page-mypage.php
@@ -168,7 +168,7 @@ if($_SESSION){
 				$jb="";
 			}
 			$cal[$n].="<td id=\"{$tmp_ymd}\" class=\"cal_td cc{$tmp_week}\">";
-			$cal[$n].="<span class=\"dy{$tmp_week}{$day_tag}\">{$tmp_day}</span>";
+			$cal[$n].="<span class=\"dy{$tmp_week}{$day_tag} cc{$tmp_week}\">{$tmp_day}</span>";
 			$cal[$n].="<span class=\"cal_i1 n1\"></span>";
 			$cal[$n].="<span class=\"cal_i2{$jb}\"></span>";
 			$cal[$n].="<span class=\"cal_i3\"></span>";
