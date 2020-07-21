4862695ac4ab5a6005620b91e8c84a0dbd7dab70
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-07-21 12:53:28 +0900

    ar

--- wp/wp-content/themes/nightparty/front-page.php
+++ wp/wp-content/themes/nightparty/front-page.php
@@ -1,45 +1,46 @@
 <?php
 get_header();
-$dat[0]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[1]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[2]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[3]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[4]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[5]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[6]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[7]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[8]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
-$dat[9]->face=get_template_directory_uri()."/img/cast/noimage.jpg";
+$now=date("Ymd",time()+32400);
 
-$now=date("Ymd",time()+54000);
-print($now);
-$res = $wpdb->get_results('SELECT * FROM wp01_0cast',ARRAY_A);
 
-$n=0;
-foreach($res as $a1){
-	$dat[$n]	=$a1;
-	if($a1["face1"] > 0){
-		$dat[$n]["face"]=get_template_directory_uri()."/img/page/".$a1["id"]."/".$a1["face1"].".jpg";			
-	}else{
-		$dat[$n]["face"]=get_template_directory_uri()."/img/page/noimage.jpg";			
-	}
-	$sch[$a1["id"]]="本日休み";
-	$n++;
+$sql=" SELECT * FROM wp01_0sch_table";
+$res0= $wpdb->get_results($sql,ARRAY_A);
+foreach($res0 as $a1){
+	$sch_table[$a1["in_out"]][$a1["name"]]=$a1["sort"];
 }
+
+$sql="SELECT * FROM wp01_0cast";
+$res= $wpdb->get_results($sql,ARRAY_A);
+
+foreach($res as $a1){
+	$dat[$a1["id"]]	=$a1;
+	if (file_exists(get_template_directory()."/img/page/{$a1["id"]}/1.jpg")) {
+		$dat[$a1["id"]]["face"]=get_template_directory_uri()."/img/page/".$a1["id"]."/1.jpg";			
+	}else{
+		$dat[$a1["id"]]["face"]=get_template_directory_uri()."/img/page/noimage.jpg";			
+	}
+		$dat[$a1["id"]]["sch"]="休み";
+		$sort[$a1["id"]]=999999;
+}
+
+var_dump($sort);
 
 
 $sql="SELECT * FROM wp01_0schedule WHERE sche_date='{$now}' ORDER BY schedule_id ASC";
-
 $res2 = $wpdb->get_results($sql,ARRAY_A);
+
 foreach($res2 as $a2){
+	if($a2["stime"] && $a2["etime"]){
+		$dat[$a1["id"]]["sch"]	="{$a2["stime"]} - {$a2["etime"]}";
+		$sort[$a1["id"]]=$sch_table["in"][$a2["stime"]];
 
-	if($a2["stime"] && $a2["etime"]):
-		$sch[$a2["cast_id"]]	="{$a2["stime"]} - {$a2["etime"]}";
-
-	else:
-		$sch[$a2["cast_id"]]	="本日休み";
-	endif;
+	}else{
+		$sch[$a2["cast_id"]]	="休み";
+		$sort[$a1["id"]]=999999;
+	}
 }
+
+
 ?>
 <div class="slide">
 	<div class="slide_img"></div>
@@ -54,14 +55,17 @@ foreach($res2 as $a2){
 </div>
 
 <div class="main_b">
-	<h1 class="main_b_title">本日の出勤キャスト</h1>
-<?for($s=0;$s<10;$s++){?>
-	<a href="<?php echo get_template_directory_uri(); ?>/person/<?=$dat[$s]["id"]?>" id="c<?=$dat[$s]["id"]?>" class="main_b_1">
-		<img src="<?=$dat[$s]["face"]?>" class="main_b_1_1">
-		<div class="main_b_1_2"><?=$dat[$s]["genji"]?><br><?=$sch[$dat[$s]["id"]]?></div>
-		<div class="main_b_1_3"></div>
+<h1 class="main_b_title">本日の出勤キャスト</h1>
+<? foreach($sort as $b1=> $b2){?>
+	<a href="<?PHP ECHO get_template_directory_uri(); ?>/person/<?PHP echo $db1?>" id="<?PHP echo $b1?>" class="main_b_1">
+		<img src="<?PHP ECHO $dat[$b1]["face"]?>" class="main_b_1_1">
+		<span class="main_b_1_2">
+			<span class="main_b_1_2_name"><?PHP echo $dat[$b1]["genji"]?></span>
+			<span class="main_b_1_2_sch"></span>
+		</span>
+		<span class="main_b_1_3"></span>
 	</a>
-<?}?>
+<? } ?>
 </div>
 <div class="main_c">
 	<div class="main_c_1">
