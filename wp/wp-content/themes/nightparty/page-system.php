e032f6c89b98c587598bdf0859b8e9aa8a9387d7
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-07-29 16:11:09 +0900

    ar

--- wp/wp-content/themes/nightparty/page-system.php
+++ wp/wp-content/themes/nightparty/page-system.php
@@ -7,14 +7,20 @@ foreach($res as $a1){
 	$sort=str_replace("SYSTEM-","",$a1["post_name"]);
 	$sys[$sort]=$a1;
 }
-print($sql);
-  
 ksort($sys);
 ?>
+
 <?foreach($sys as $a1){?>
+<?if($a1["post_content"]){?>
 <div class="sys_box">
 <span class="sys_box_ttl"><?=$a1["post_title"]?></span><br>
 <span class="sys_box_log"><?=$a1["post_content"]?></span><br>
+</div>
+<?}?>
+<?}?>
+<?if(!$sys){?>
+<div class="sys_box">
+<span class="sys_box_log">情報はまだありません</span><br>
 </div>
 <?}?>
 
