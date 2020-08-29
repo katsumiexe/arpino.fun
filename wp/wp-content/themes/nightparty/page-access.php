e1e31f5e30bcf143b49e6610a760aaf49217d746
katsumiexe <counterpost2016@gmail.com>
2020-08-29 15:00:41 +0900

    ar

--- wp/wp-content/themes/nightparty/page-access.php
+++ wp/wp-content/themes/nightparty/page-access.php
@@ -15,6 +15,7 @@ Template Name: access
 		<span class="footmark_text">ACCESS</span>
 	</div>
 </div>
+
 <div class="access_table">
 	<div class="access_map">
 		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d721.6854943560575!2d139.70315573035856!3d35.69525939181556!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd7751357e3fdec4b!2z44OX44Op44Km44OH44Kj44Ki!5e0!3m2!1sja!2sjp!4v1596258206301!5m2!1sja!2sjp" width="650" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" class="access_map_in"></iframe>
