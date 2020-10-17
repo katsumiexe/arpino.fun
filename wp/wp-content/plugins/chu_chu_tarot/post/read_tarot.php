c4d28dd78362603e2d11ef35100f9a547ee01743
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-10-17 15:04:40 +0900

    ar

--- wp/wp-content/plugins/chu_chu_tarot/post/read_tarot.php
+++ wp/wp-content/plugins/chu_chu_tarot/post/read_tarot.php
@@ -4,27 +4,42 @@ ini_set('display_errors',1);
 require_once ("../../../../wp-load.php");
 global $wpdb;
 
-$tarot_id		=$_POST["tarot_id"];
-$n_r			=$_POST["n_r"];
+$tarot_id	=$_POST["tarot_id"];
+$n_r		=$_POST["n_r"];
 $s=0;
+
+$gp=1;
 
 $position[1]="正";
 $position[0]="逆";
+$rev[0]		="chu_img_rev";
 
-$rev[0]="chu_img_rev";
 
+$sql	 ="SELECT * FROM tarot_group";
+$sql	 .=" WHERE group_id='{$gp}'";
+$sql	 .=" AND del=0";
+$group = $wpdb->get_row($sql,ARRAY_A);
 
+$sql	 ="SELECT sort, oracle_name FROM tarot_oracle";
+$sql	 .=" WHERE oracle_id='{$gp}'";
+$sql0 = $wpdb->get_results($sql,ARRAY_A);
+foreach($sql0 as $a1){
+	$dat[$a1["sort"]]=$a1["oracle_name"];
+}
 
 $sql	 ="SELECT * FROM tarot_data";
-$sql	 .=" WHERE (tarot_id='{$tarot_id[0]}' && n_r='{$n_r[0]}' && result='0')";
+$sql	 .=" WHERE (group_id='{$gp}')";
+$sql	 .=" AND(";
+$sql	 .="(tarot_id='{$tarot_id[0]}' && n_r='{$n_r[0]}' && result='0')";
 $sql	 .=" OR (tarot_id='{$tarot_id[1]}' && n_r='{$n_r[1]}' && result='1')";
 $sql	 .=" OR (tarot_id='{$tarot_id[2]}' && n_r='{$n_r[2]}' && result='2')";
+$sql	 .=" )";
+
 $sql1 = $wpdb->get_results($sql,ARRAY_A);
 
 foreach($sql1 as $a1){
 	$dat[$a1["result"]]=str_replace("\n","<br>",$a1["tarot_log"]);
 }
-
 
 $sql	 ="SELECT * FROM tarot_base";
 $sql	 .=" WHERE id='{$tarot_id[0]}' OR id='{$tarot_id[1]}' OR id='{$tarot_id[2]}'";
@@ -46,7 +61,7 @@ for($n=0;$n<3;$n++){
 $html.="<div class=\"chu_ans\"><img id=\"img-{$n}\" src=\"../../../../wp/wp-content/plugins/chu_chu_tarot/img/cardimg_{$tarot_id[$n]}.jpg\" class=\"chu_ans_img {$rev[$n_r[$n]]}\">";
 $html.="<div class=\"chu_ans_ttl\"><span id=\"name_j-{$n}\">{$dat2[$tarot_id[$n]]["name_j"]}</span>/<span id=\"name_e-{$n}\">{$dat2[$tarot_id[$n]]["name_e"]}</span></div>";
 $html.="<div class=\"chu_ans_msg\"><span id=\"mean-{$n}\">{$dat2[$tarot_id[$n]]["mean"]}</span></div>";
-$html.="<div id=\"log-{$n}\" class=\"chu_ans_comm\">{$dat[$n]}</div>";
+$html.="<div id=\"log-{$n}\" class=\"chu_ans_comm\">{$dat[$tarot_id[$n]}</div>";
 $html.="<div id=\"rev-{$n}\" class=\"chu_face f$n_r[$n]\">{$position[$n_r[$n]]}</div></div>";
 }
 
