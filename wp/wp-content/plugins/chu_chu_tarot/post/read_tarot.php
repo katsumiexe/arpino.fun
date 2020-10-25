91d18bb5d0899404d82ec6ed83b567957d3233af
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-10-25 15:24:48 +0900

    ar

--- wp/wp-content/plugins/chu_chu_tarot/post/read_tarot.php
+++ wp/wp-content/plugins/chu_chu_tarot/post/read_tarot.php
@@ -37,27 +37,42 @@ foreach($sql1 as $a1){
 	$dat[$a1["result"]]=str_replace("\n","<br>",$a1["tarot_log"]);
 }
 
+//$html="■{$gp}■{$group["oracle"]}■";
+
 $s=0;
+for($n=1;$n<$group["oracle"];$n++){
+	$app.=" OR id='{$tarot_id[$n]}'";
+}
+
 $sql	 ="SELECT * FROM tarot_base";
-$sql	 .=" WHERE id='{$tarot_id[0]}' OR id='{$tarot_id[1]}' OR id='{$tarot_id[2]}'";
+$sql	 .=" WHERE id='{$tarot_id[0]}'";
+$sql	 .=$app;
+
 $sql2 = $wpdb->get_results($sql,ARRAY_A);
 
 foreach($sql2 as $a2){
 	$dat2[$a2["id"]]=$a2;
+}
+
+for($s=0;$s<$group["oracle"];$s++){
 	if($n_r[$s]==1){
-		$dat2[$a2["id"]]["mean"]=$a2["mean_1"];
+		$mean[$tarot_id[$s]]=$dat2[$tarot_id[$s]]["mean_1"];
+$html.="{$s}■{$n_r[$s]}□";
 
 	}else{
-		$dat2[$a2["id"]]["mean"]=$a2["mean_0"];
+		$mean[$tarot_id[$s]]=$dat2[$tarot_id[$s]]["mean_0"];
+$html.="{$s}▲{$n_r[$s]}△";
 	}
-	$s++;
 }
 
 
+
+$html.=$sql;
+	
 for($n=0;$n<$group["oracle"];$n++){
 $html.="<div class=\"chu_ans\"><img id=\"img-{$n}\" src=\"../../../../wp/wp-content/plugins/chu_chu_tarot/img/cardimg_{$tarot_id[$n]}.jpg\" class=\"chu_ans_img {$rev[$n_r[$n]]}\">";
 $html.="<div class=\"chu_ans_ttl\"><span id=\"name_j-{$n}\">{$dat2[$tarot_id[$n]]["name_j"]}</span>/<span id=\"name_e-{$n}\">{$dat2[$tarot_id[$n]]["name_e"]}</span></div>";
-$html.="<div class=\"chu_ans_msg\"><span id=\"mean-{$n}\">{$dat2[$tarot_id[$n]]["mean"]}</span></div>";
+$html.="<div class=\"chu_ans_msg\"><span id=\"mean-{$n}\">{$mean[$tarot_id[$n]]}</span></div>";
 $html.="<div id=\"log-{$n}\" class=\"chu_ans_comm\">{$dat[$n]}</div>";
 $html.="<div id=\"rev-{$n}\" class=\"chu_face f$n_r[$n]\">{$position[$n_r[$n]]}</div></div>";
 }
