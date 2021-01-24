4aa67a1c0fd98a135be7e161d7291ce1b6fae735
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-01-24 14:49:17 +0900

    ar

--- wp/wp-content/themes/nightparty/post/blog_set.php
+++ wp/wp-content/themes/nightparty/post/blog_set.php
@@ -41,6 +41,7 @@ if(!$dat){
 	$sql	 ="INSERT INTO wp01_terms(`name`,`slug`)";
 	$sql	.=" VALUES('{$_SESSION["genji"]}','{$cast_id}')";
 	$wpdb->query($sql);
+
 	$terms_id=$wpdb->insert_id;
 
 	$sql	 ="INSERT INTO wp01_term_taxonomy(`term_id`,`taxonomy`)";
@@ -98,28 +99,24 @@ if($chg){
 	$wpdb->query($sql);
 
 }else{
+
 	$sql="INSERT INTO wp01_posts ";
 	$sql.="(post_author, post_date, post_date_gmt, post_content, post_title, post_status, post_modified, post_modified_gmt, comment_status, ping_status, post_name, guid, post_type, post_parent)";
 	$sql.="VALUES";
 	$sql.="('{$cast_id}','{$date_jst}','{$date_gmt}','{$log}','{$ttl}','publish','{$date_jst}','{$date_gmt}'";
 	$sql.=",'open','open','post','{$updir['baseurl']}/{$cast_id}/','post','0')";
+	$wpdb->query($sql);
 
-	$wpdb->query($sql);
 	$tmp_auto=$wpdb->insert_id;
+
+
 	$tmp_auto2=$tmp_auto++;
-/*
-	$sql="INSERT INTO wp01_posts ";
-	$sql.="(post_author, post_date, post_date_gmt, post_content, post_title, post_status, post_modified, post_modified_gmt, comment_status, ping_status, post_name, guid, post_type, post_parent)";
-	$sql.="VALUES";
-	$sql.="('{$cast_id}','{$date_jst}','{$date_gmt}','{$log}','{$ttl}','inherit','{$date_jst}','{$date_gmt}'";
-	$sql.=",'closed','closed','{$tmp_auto}-revision-v1','{$updir['baseurl']}/{$cast_id}/{$tmp_auto}-revision-v1/','revision','{$tmp_auto}')";
-	$wpdb->query($sql);
-*/
+
 	if($img_code){
 		$tmp_img=$tmp_auto2;
 
 		$sql="INSERT INTO wp01_posts ";
-		$sql.="(post_author,post_date,post_date_gmt,post_title,post_status,post_modified,post_modified_gmt, comment_status,ping_status,post_name,guid,post_type,post_mime_type";
+		$sql.="(post_author,post_date,post_date_gmt,post_title,post_status,post_modified,post_modified_gmt, comment_status,ping_status,post_name,guid,post_type,post_mime_type)";
 		$sql.="VALUES";
 		$sql.="('{$cast_id}','{$date_jst}','{$date_gmt}','img_{$tmp_auto2}','inherit','{$date_jst}','{$date_gmt}'";
 		$sql.=",'open','closed','img_{$tmp_auto2}','{$updir['baseurl']}/np{$cast_id}/img_{$tmp_auto2}.png','attachment','image/png')";
@@ -141,30 +138,6 @@ if($chg){
 
 		$link	="../../../../wp-content/uploads/np{$cast_id}/img_{$tmp_auto2}.png";
 		$img_64	= imagecreatefromstring(base64_decode($img_code));	
-/*
-		$img2 		= imagecreatetruecolor(600,600);
-		$new_img	= imagecreatefromstring(base64_decode($img_code));	
-
-		$tmp_top	=floor(((($vw_base*10-$img_top)*10)/$vw_base)*100/$img_zoom);
-		$tmp_left	=floor(((($vw_base*10-$img_left)*10)/$vw_base)*100/$img_zoom);
-
-		$tmp_width	=floor(600/($img_zoom/100));
-		$tmp_height	=floor(600/($img_zoom/100));
-
-		if($img_rote ==90){
-			$new_img = imagecreatefromstring(base64_decode($img_code));	
-			$img = imagerotate($new_img, 270, 0, 0);
-
-		}elseif($img_rote ==270){
-			$new_img = imagecreatefromstring(base64_decode($img_code));
-			$img = imagerotate($new_img, 90, 0, 0);
-
-		}else{
-			$img = imagecreatefromstring(base64_decode($img_code));
-		}
-
-		ImageCopyResampled($img2, $img, 0, 0, $tmp_left, $tmp_top, 600, 600, $tmp_width, $tmp_height);
-*/
 		imagepng($img_64,$link);
 	}elseif($album){
 		$tmp_img=$album;
