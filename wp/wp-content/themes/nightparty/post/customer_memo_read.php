ff86b6a97e164353614406fdc4708f991a5c2aa7
katsumiexe <counterpost2016@gmail.com>
2021-02-15 21:27:33 +0900

    ar

--- wp/wp-content/themes/nightparty/post/customer_memo_read.php
+++ wp/wp-content/themes/nightparty/post/customer_memo_read.php
@@ -19,16 +19,22 @@ $sql	 .=" ORDER BY `date` DESC";
 $now=date("Y-m-d H:i");
 $dat0 = $wpdb->get_results($sql,ARRAY_A );
 
-foreach($dat0 AS $dat1){
-	$dat1["log"]=str_replace("\n","<br>",$dat1["log"]);
+if($dat0){
+	foreach($dat0 AS $dat1){
+		$dat1["log"]=str_replace("\n","<br>",$dat1["log"]);
 
-	$dat.="<tr id=\"tr_memo_detail{$dat1["id"]}\"><td class=\"customer_memo_td1\">";
-	$dat.="<div class=\"customer_memo_date\">{$dat1["date"]}</div>";
-	$dat.="<div id=\"m_chg{$dat1["id"]}\" class=\"customer_memo_chg\"></div>";
-	$dat.="<div id=\"m_del{$dat1["id"]}\" class=\"customer_memo_del\"></div>";
-	$dat.="</td></tr><tr id=\"tr_memo_log{$dat1["id"]}\"><td id=\"m_log{$dat1["id"]}\" class=\"customer_memo_td2\">";
-	$dat.="{$dat1["log"]}";
-	$dat.="</td></tr>";
+		$dat.="<tr id=\"tr_memo_detail{$dat1["id"]}\"><td class=\"customer_memo_td1\">";
+		$dat.="<div class=\"customer_memo_date\">{$dat1["date"]}</div>";
+		$dat.="<div id=\"m_chg{$dat1["id"]}\" class=\"customer_memo_chg\"></div>";
+		$dat.="<div id=\"m_del{$dat1["id"]}\" class=\"customer_memo_del\"></div>";
+		$dat.="</td></tr><tr id=\"tr_memo_log{$dat1["id"]}\"><td id=\"m_log{$dat1["id"]}\" class=\"customer_memo_td2\">";
+		$dat.="{$dat1["log"]}";
+		$dat.="</td></tr>";
+	}
+}else{
+		$dat.="<tr><td class=\"customer_memo_td1\">まだ何もありません</td></tr>";
+
+
 }
 
 echo $dat;
