ad679b628845bf2cc89a56f352e09fec190035f7
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-02-24 09:48:41 +0900

    ar

--- ngt/manage.php
+++ ngt/manage.php
@@ -7,12 +7,11 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="./js/jquery.ui.touch-punch.min.js?t=<?=time()?>"></script>
-<script src="./js/main.js?t=<?=time()?>"></script>
+<script src="./js/manage.js?t=<?=time()?>"></script>
 
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 <link rel="stylesheet" href="./css/style.css?t=<?=time()?>">
-<link rel="stylesheet" href="./css/person.css?t=<?=time()?>">
-
+<link rel="stylesheet" href="./css/manage.css?t=<?=time()?>">
 <style>
 @font-face {
 	font-family: at_icon;
@@ -38,40 +37,27 @@
 </head>
 <body class="body">
 <header class="head">
-	<div class="head_top">
-	</div>
-	<div class="head_in">
-		<ul id="menu-main" class="menu">
-			<li class="menu-item"><a href="./index.php">Top Page</a></li>
-			<li class="menu-item"><a href="./system.php">Sytem</a></li>
-			<li class="menu-item"><a href="./cast.php">Cast</a></li>
-			<li class="menu-item"><a href="./castblog.php">Blog</a></li>
-			<li class="menu-item"><a href="./access.php">Access</a></li>
-			<li class="menu-item"><a href="./recruit.php">Recruit</a></li>
-		</ul>
-		<div class="head_menu">
-			<div class="menu_a"></div>
-			<div class="menu_b"></div>
-			<div class="menu_c"></div>
-		</div>
-		<div class="head_tel"></div>
-	</div>
 </header>
 <div class="left">
-<div class="menu">登録</div>
-<div class="menu">スタッフ</div>
-<div class="menu">スケジュール</div>
-<div class="menu">ブログ</div>
-<div class="menu">コンテンツ</div>
-<div class="menu">EasyTalk</div>
-<div class="menu">お知らせ</div>
-<div class="menu">コンフィグ</div>
+<ul class="menu_ul">登録</div>
+	<li id="regist" class="menu">登録</li>
+	<li id="staff" class="menu">スタッフ</li>
+	<li id="sche" class="menu">スケジュール</li>
+	<li id="blog" class="menu">ブログ</li>
+	<li id="contents" class="menu">コンテンツ</li>
+	<li id="easytalk" class="menu">EasyTalk</li>
+	<li id="notice" class="menu">お知らせ</li>
+	<li id="config" class="menu">コンフィグ</li>
+</ul>
+<div class="head_menu">
+	<div class="menu_a"></div>
+	<div class="menu_b"></div>
+	<div class="menu_c"></div>
+</div>
 </div>
 <div class="main">
-
-
+<?=include_once('./library/manage_regist.php');?>
 </div>
-
 <footer class="footer">
 </footer>
 </html>
