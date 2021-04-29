f59f8b8ad08a64d4c844035d6277a15c267eb4b3
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-04-29 10:56:31 +0900

    ar

--- ngt/admin/admin_notice.php
+++ ngt/admin/admin_notice.php
@@ -5,6 +5,10 @@ $sql	.=" ORDER BY `date` DESC";
 $sql	.=" LIMIT 10";
 if($result = mysqli_query($mysqli,$sql)){
 	while($row = mysqli_fetch_assoc($result)){
+
+		$row["log"]=str_replace("\n","<br>",$row["log"]);
+		$row["date"]=str_replace("-",".",substr($row["date"],0,16));
+
 		$sql	 ="SELECT id, genji, cast_group, K.status FROM wp01_0notice_ck AS K";
 		$sql	.=" LEFT JOIN  wp01_0cast AS C ON K.cast_id=C.id";
 		$sql	.=" WHERE C.del=0";
@@ -12,6 +16,7 @@ if($result = mysqli_query($mysqli,$sql)){
 		if($result2 = mysqli_query($mysqli,$sql)){
 			while($row2 = mysqli_fetch_assoc($result2)){
 				$dat2[$row["id"]][]=$row2;
+
 			}
 		}
 		$dat[]=$row;
@@ -32,7 +37,6 @@ if($result = mysqli_query($mysqli,$sql)){
 
 		}else{
 			$row["user_name"]=$row["name"];
-		
 		}
 		$staff_dat[]=$row;
 	}
@@ -87,80 +91,195 @@ input[type=radio]:checked + label{
 
 .cate_title{
 	display			:inline-block;
-	width			:200px;
-	height			:40px;
-	line-height		:40px;
+	width			:210px;
+	height			:30px;
+	line-height		:30px;
 	font-size		:15px;
-	background		:#d00000;
 	padding-left	:5px;
 	text-align		:left;
-	margin			:10px 3px 0 3px;
+	margin-left		:5px;
 	color			:#fafafa;
+	font-weight		:600;
 }
 
-.cate_box{
-	display			:inline-block;
-	width			:200px;
-	height			:40px;
-	line-height		:40px;
-	font-size		:15px;
-	background		:#d00000;
-	padding-left	:5px;
-	text-align		:left;
-	margin			:10px 3px 0 3px;
-	color			:#fafafa;
+.cate_ul{
+	width			:240px;
+	margin			:10px 5px;
+	border-radius	:5px;
+	box-shadow		:3px 3px 3px rgba(30,30,30,0.5);
+	padding			:5px;
 }
-.cate_list{
+
+.cate_li{
 	display			:inline-block;
-	width			:200px;
-	height			:35px;
-	line-height		:40px;
-	font-size		:15px;
-	background		:#fafafa;
+	width			:225px;
+	height			:30px;
+	line-height		:30px;
+	margin			:0 0 1px 10px;
 	padding-left	:5px;
-	text-align		:left;
-	border-bottom	:1px solid #202020;
-	margin			:3px;
+	font-size		:15px;
+	color			:#ffffff;	
+}
+
+.c_pink{
+	background:#ff7090;
+}
+.c_pink2{
+	background:#ffa0c0;
+}
+.c_pink2:hover{
+	background:#ff7898;
+}
+
+
+.c_blue{
+	background:#4068e0;
+}
+.c_blue2{
+	background:#80a8e0;
+}
+
+.c_blue2:hover{
+	background:#6088e0;
+}
+
+
+.c_green{
+	background:#00a010;
+}
+
+.c_green2{
+	background:#30e040;
+}
+
+.c_green2:hover{
+	background:#00b020;
+}
+
+
+.notice_table{
+	margin			:5px auto;
+	background		:#fafafa;
+	border			:1px solid #303030;
+}
+
+.notice_top{
+	background		:#b0c0d0;
+	border			:1px solid #303030;
+	color			:#fafafa;
+	height			:18px;
+	line-height		:18px;
+	padding-left	:5px;
+	font-size		:13px;
+}
+
+.notice_list{
+	border			:1px solid #303030;
+	color			:#202020;
+	height			:20px;
+	line-height		:20px;
+	padding-left	:5px;
+	font-size		:14px;
+}
+
+.notice_hidden{
+	display:none;
+}
+
+.w100{
+	width:100px;
+}
+.w120{
+	width:120px;
+}
+
+.w140{
+	width:140px;
+}
+
+.w240{
+	width:240px;
+}
+
+.tr_list{
+	cursor:pointer;
+}
+
+.tr_list:hover{
+	background:#f0e8d0;
+}
+
+.notice_log{
+	margin			:5px auto;
+	width			:760px;
+	height			:400px;
+	padding			:10px;
+	font-size		:14px;
+	line-height		:24px;
+	border			:1px solid #303030;
+	border-radius	:5px;
+	background		:#fafafa;
+	overflow-y		:scroll;
 }
 
 -->
 </style>
 <script>
 $(function(){ 
+	$('.tr_list').on('click',function(){ 
+		var Tmp=$(this).children('.notice_hidden').html();
+		$('.notice_log').html(Tmp);
+	});
 });
 </script>
+
 <header class="head">
 </header>
 <div class="wrap">
 	<div class="main_box">
 		<table class="notice_table">
 			<tr>
-				<td class="notice_top">日時</td>
-				<td class="notice_top">カテゴリ</td>
-				<td class="notice_top">投稿者</td>
-				<td class="notice_top">件名</td>
-				<td class="notice_top">グループ</td>
+				<td class="notice_top w140">日時</td>
+				<td class="notice_top w240">件名</td>
+				<td class="notice_top w100">投稿者</td>
+				<td class="notice_top w120">カテゴリ</td>
+				<td class="notice_top w120">グループ</td>
 			</tr>
 			<?for($n=0;$n<$count_dat;$n++){?>
-			<tr>
-				<td class="notice_140"><?=$dat[$n]["date"]?></td>
-				<td class="notice_80"><?=$dat[$n]["date"]?></td>
-				<td class="notice_140"><?=$dat[$n]["date"]?></td>
-				<td class="notice_250"><?=$dat[$n]["date"]?></td>
-				<td class="notice_80"><?=$dat[$n]["date"]?></td>
+			<tr class="tr_list">
+				<td class="notice_list"><?=$dat[$n]["date"]?></td>
+				<td class="notice_list"><?=$dat[$n]["title"]?></td>
+				<td class="notice_list"><?=$dat[$n]["writer"]?></td>
+				<td class="notice_list"><?=$dat[$n]["category"]?></td>
+				<td class="notice_list"><?=$dat[$n]["group"]?></td>
+				<td class="notice_hidden"><?=$dat[$n]["log"]?></td>
 			</tr>
 			<?}?>
 		</table>
+		<div class="notice_log"></div>
+
 	</div>
+
 	<div class="sub_box">
-		<div class="cate_title">カテゴリー</div>
-		<?foreach($tag["cast_group"] as $a1 => $a2){?>
-			<span class="cate_list"><?=$a1?><?=$a2?></span><br>
-		<?}?>
-		<div class="cate_title">スタッフ</div>
-		<?foreach($staff_dat as $a1 => $a2){?>
-			<span class="cate_list"><?=$a1?><?=$a2["user_name"]?></span><br>
-		<?}?>
+		<ul class="cate_ul c_blue">
+			<li class="cate_title">MENU</li>
+			<li class="cate_li c_blue2">投稿</li>
+			<li class="cate_li c_blue2">検索</li>
+			<li class="cate_li c_blue2">チェック</li>
+		</ul>
+
+		<ul class="cate_ul c_green">
+			<li class="cate_title">カテゴリー</li>
+			<?foreach($tag["cast_group"] as $a1 => $a2){?>
+			<li class="cate_li c_green2"><?=$a2?></li><?}?>
+		</ul>
+
+		<ul class="cate_ul c_pink">
+			<li class="cate_title">スタッフ</li>
+			<?foreach($staff_dat as $a1 => $a2){?>
+			<li class="cate_li c_pink2"><?=$a2["user_name"]?></li><?}?>
+		</ul>
+
 	</div>
 </div>
 <footer class="foot"></footer>
