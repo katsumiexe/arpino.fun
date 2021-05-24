df7404fd3840ec5129b323068f331c02fa906fdb
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-05-24 16:06:26 +0900

    ar

--- komamemo/index.php
+++ komamemo/index.php
@@ -40,17 +40,14 @@ if($cast["id"]){
 		}
 	}
 }
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="./js/jquery.ui.touch-punch.min.js?t=<?=time()?>"></script>
@@ -59,7 +56,6 @@ if($cast["id"]){
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 <link rel="stylesheet" href="./css/style.css?t=<?=time()?>">
 <style>
-
 .body{
 	text-align			:center;
 	margin				:0;
