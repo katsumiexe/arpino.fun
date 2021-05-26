6b0357829867e12e96111bb71ad0e55c152553dc
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-05-26 09:22:58 +0900

    arar

--- komamemo/index.php
+++ komamemo/index.php
@@ -1,31 +1,41 @@
 <?php
 include_once('./library/sql.php');
 
-if($_REQUEST["logout"]){
-	session_destroy();
-	$cast="";
 
-}elseif($_POST["set"] && !$cast["id"]){
+$masu_l[1]="一";
+$masu_l[2]="二";
+$masu_l[3]="三";
+$masu_l[4]="四";
+$masu_l[5]="五";
+$masu_l[6]="六";
+$masu_l[7]="七";
+$masu_l[8]="八";
+$masu_l[9]="九";
+
+if($_POST["logout"]){
+	session_destroy();
+}elseif($_POST["set"]){
 	$title	=$_POST["title"];
 	$place	=$_POST["place"];
 	$first	=$_POST["first"];
 	$second	=$_POST["second"];
 	$kami	=$_POST["kami"];
-
 	if(!$title) $title="対局";
 	if(!$place) $place="将棋会館";
 	if(!$first) $first="名無しさん";
 	if(!$second) $second="名無し先生";
 	include_once('./library/start.php');
 
-	$cast["id"]			=$tmp_auto;
-	$cast["title"]		=$title;
-	$cast["place"]		=$place;
-	$cast["first"]		=$first;
-	$cast["second"]		=$second;
-	$cast["cast_time"]	=time();
-	$_SESSION			=$cast;
+	$cast["id"]		=$tmp_auto;
+	$cast["title"]	=$title;
+	$cast["place"]	=$place;
+	$cast["first"]	=$first;
+	$cast["second"]	=$second;
+	$cast["cast_time"]=time();
+	$_SESSION=$cast;
 }
+
+
 if($cast["id"]){
 	$sql="SELECT * FROM komamemo_base";
 	if($result = mysqli_query($mysqli,$sql)){
@@ -56,75 +66,44 @@ if($cast["id"]){
 
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 <link rel="stylesheet" href="./css/style.css?t=<?=time()?>">
-<style>
 
-<?for($e=1;$e<10;$e++){?>
-<?$e1=69.5-8.5*($e-1);?>
-.c<?=$e?>{left:<?=$e1?>vw;}
-<?}?>
-
-<?for($e=1;$e<10;$e++){?>
-<?$e1=0.5+9.5*($e-1);?>
-.l<?=$e?>{top:<?=$e1?>vw;}
-<?}?>
-</style>
 </head>
 <body class="body">
 <div class="main">
-<?if($cast["id"]){?>
+<?if($base){?>
+
 <table class="waku">
 <tr>
-<td class="waku_top" colspan="12"></td>
-</tr>
-<tr>
-	<td></td>
-	<td class="waku_column">九</td>
-	<td class="waku_column">八</td>
-	<td class="waku_column">七</td>
-	<td class="waku_column">六</td>
-	<td class="waku_column">五</td>
-	<td class="waku_column">四</td>
-	<td class="waku_column">三</td>
-	<td class="waku_column">二</td>
-	<td class="waku_column">一</td>
-	<td></td>
-	<td></td>
+<td class="waku_top" colspan="11"></td>
 </tr>
 <tr>
 	<td class="waku_left">
-		<span class="m_koma"><img src="koma/k90.png" class="koma_img_r u2"><span class="m_koma_c">6</span></span>
-		<span class="m_koma"><img src="koma/k80.png" class="koma_img_r u2"></span>
-		<span class="m_koma"><img src="koma/k70.png" class="koma_img_r u2"><span class="m_koma_c">2</span></span>
-		<span class="m_koma"><img src="koma/k60.png" class="koma_img_r u2"></span>
-		<span class="m_koma"><img src="koma/k50.png" class="koma_img_r u2"></span>
-		<span class="m_koma"><img src="koma/k40.png" class="koma_img_r u2"></span>
-		<span class="m_koma"><img src="koma/k30.png" class="koma_img_r u2"></span>
+		<span class="m_koma"><img src="koma/k90.png" class="koma_img_r u1"><span class="m_koma_c">6</span></span>
+		<span class="m_koma"><img src="koma/k80.png" class="koma_img_r u1"></span>
+		<span class="m_koma"><img src="koma/k70.png" class="koma_img_r u1"><span class="m_koma_c">2</span></span>
+		<span class="m_koma"><img src="koma/k60.png" class="koma_img_r u1"></span>
+		<span class="m_koma"><img src="koma/k50.png" class="koma_img_r u1"></span>
+		<span class="m_koma"><img src="koma/k40.png" class="koma_img_r u1"></span>
+		<span class="m_koma"><img src="koma/k30.png" class="koma_img_r u1"></span>
 	</td>
 
 	<td class="waku_main" colspan="9">
 		<div class="ban">
-			<?for($n=1;$n<10;$n++){?>
-				<?for($s=1;$s<10;$s++){?>
-					<div class="masu" style="top:<?=($n-1)*9.5+0.5?>vw; left:<?=($s-1)*8.5+0.5?>vw;"></div>
-				<?}?>
+			<?for($n=9;$n>0;$n--){?>
+			<div class="masu_c"><?=$n?></div>
 			<?}?>
+			<div class="masu_cl"></div>
+			<?for($n=1;$n<10;$n++){?>
+				<?for($s=9;$s>0;$s--){?>
+					<div class="masu" cc="<?=$s?>" ll="<?=$n?>"></div>
+				<?}?>
+				<div class="masu_l"><?=$masu_l[$n]?></div>
+			<?}?>
+
 			<?for($t=1;$t<41;$t++){?>
-				<span id="koma<?=$t?>"  cc="<?=$dat[$t]["column"]?>" ll="<?=$dat[$t]["line"]?>" class="koma c<?=$dat[$t]["colum"]?> l<?=$dat[$t]["line"]?> s<?=$dat[$t]["style"]?> u<?=$dat[$t]["user"]?>"><img src="koma/<?=$base[$t]["img"]?><?=$dat[$t]["style"]?>.png" class="koma_img"></span>
+				<span id="koma<?=$t?>" cc="<?=$dat[$t]["colum"]?>" ll="<?=$dat[$t]["line"]?>" class="koma c<?=$dat[$t]["colum"]?> l<?=$dat[$t]["line"]?> s<?=$dat[$t]["style"]?> u<?=$dat[$t]["user"]?>"><img src="koma/<?=$base[$t]["img"]?><?=$dat[$t]["style"]?>.png" class="koma_img"></span>
 			<? } ?>
 		</div>
-	</td>
-
-
-	<td class="waku_line">
-		<span class="waku_line_in">1</span>
-		<span class="waku_line_in">2</span>
-		<span class="waku_line_in">3</span>
-		<span class="waku_line_in">4</span>
-		<span class="waku_line_in">5</span>
-		<span class="waku_line_in">6</span>
-		<span class="waku_line_in">7</span>
-		<span class="waku_line_in">8</span>
-		<span class="waku_line_in">9</span>
 	</td>
 	<td class="waku_right">
 		<span class="m_koma"><img src="koma/k30.png" class="koma_img_r"></span>
@@ -137,11 +116,18 @@ if($cast["id"]){
 	</td>
 </tr>
 <tr>
-<td class="waku_bottom" colspan="12"><a href="./index.php?logout=1;" style="font-size:4vw;">あいうえお</a></td>
+<td class="waku_bottom" colspan="11"><a href="./index.php?logout=1;">あいうえお</a></td>
 </tr>
 <tr>
-<td class="waku_comm" colspan="12">
-<div class="hist"></div>
+<td class="waku_comm" colspan="11">
+<div class="hist" style="font-size:4vw;">
+駒縦<span id="k_size_h" style="font-size:4vw;"></span><br>
+駒横<span id="k_size_w" style="font-size:4vw;"></span><br>
+桝縦<span id="m_size_h" style="font-size:4vw;"></span><br>
+桝横<span id="m_size_w" style="font-size:4vw;"></span><br>
+
+
+</div>
 <div class="memo"></div>
 </td>
 </tr>
--- komamemo/js/main.js
+++ komamemo/js/main.js
@@ -1,28 +1,58 @@
 $(function(){
-	var KB[0,1,2,3,3,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9];
-	var Te_Couunt=0;
+	KH=$('#koma1').height();
+	KW=$('#koma1').width();
+	
+	MH=$('.masu').height();
+	MW=$('.masu').width();
+
+	$('#k_size_h').text(KH);
+	$('#k_size_w').text(KW);
+	$('#m_size_h').text(MH);
+	$('#m_size_w').text(MW);
+
+console.log(KH);
+console.log(KW);
+
+	var KB=[0,1,2,3,3,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9];
+	var Coma=["▲","△"];
+	var Masu_L=["","一","二","三","四","五","六","七","八","九"];
+
+	var Te_Count=0;
+	var Player='';
+	var Opponent='';
+
+
+	var Tmp_ch	="";
+	var Tmp_T	="";
+	var Tmp_L	="";
+	var TmpId	="";
+	var Tmp2	="";
+	var Tmp		="";
+	var Tmp_cc	="";
+	var Tmp_ll	="";
 
 	$('.koma').on('click',function(){
 
-		Playar		=Te_Count % 2
+		Player		=Te_Count % 2
 		Opponent	=(Te_Count+1) % 2
 
 		if($('.koma_on').hasClass('u' + Player) && $(this).hasClass('u' + Opponent)){
 
-			Tmp_T=$(this).css('top');
-			Tmp_L=$(this).css('left');
-
+			TmpId	=$(this).attr('id').replace('c','');
 			Tmp2	=$(this).attr('id').replace('koma','');
 			Tmp		=$('.koma_on').attr('id').replace('koma','');
+
 			Tmp_cc	=$(this).attr('cc');
 			Tmp_ll	=$(this).attr('ll');
 
-			$('.koma').removeClass('koma_on');
-			$('.koma_on').animate({'left':Tmp_L,'top':Tmp_T},400);
+			Tmp_C	=72-$(this).attr('cc')*8;
+			Tmp_L	=$(this).attr('ll') *9 -5 ;
+			$('.koma_on').animate({'left':Tmp_C+'vw','top':Tmp_L+'vw'},300);
+
 			$(this).fadeOut(400);
+			$('.koma').removeClass('koma_on');
 
 			if($(this).hasClass('s0')){
-
 				if(	KB[Tmp] == 9 ||	KB[Tmp] == 8){
 
 					//■恐怖無条件成
@@ -35,12 +65,13 @@ $(function(){
 
 					//■恐怖成確認
 					}else if(Tmp_ll < 4 && Player ==0){
-					
+						Tmp_ch=2;
+
 					}else if(Tmp_ll > 6 && Player ==1){
+						Tmp_ch=2;
 
 					}
 				} else if(KB[Tmp] == 7){
-
 					//■K無条件成
 					if(Tmp_ll < 3 && Player ==0){
 						Tmp_ch=1;
@@ -51,21 +82,25 @@ $(function(){
 
 					//■K成確認
 					}else if(Tmp_ll == 3 && Player ==0){
-					
+						Tmp_ch=2;
+
 					}else if(Tmp_ll == 7 && Player ==1){
+						Tmp_ch=2;
 
 					}
 
-				} else	if(	KB[Tmp] == 3 ||	KB[Tmp] == 4 ||	KB[Tmp] == 6){
+				}else	if(	KB[Tmp] == 3 ||	KB[Tmp] == 4 ||	KB[Tmp] == 6){
 					if(Tmp_ll < 4 && Player ==0){
-					
+						Tmp_ch=2;
+
 					}else if(Tmp_ll > 6 && Player ==1){
+						Tmp_ch=2;
 
 					}
 				}
 			}
 
-
+			Te_Count++;
 			$.post({
 				url:"./post/move_get.php",
 				data:{
@@ -74,11 +109,23 @@ $(function(){
 					'move_ll'	:Tmp_ll,
 					'move_cc'	:Tmp_cc,
 					'move_ch'	:Tmp_ch,
+					'count'		:Te_Count,
 				},
-				dataType: 'json',
 
 			}).done(function(data, textStatus, jqXHR){
-				Te_Count++;
+				TmpId	="";
+				Tmp2	="";
+				Tmp		="";
+				Tmp_cc	="";
+				Tmp_ll	="";
+				Tmp_ch	="";
+
+				Tmp_C	="";
+				Tmp_L	="";
+
+			}).fail(function(jqXHR, textStatus, errorThrown){
+				console.log(textStatus);
+				console.log(errorThrown);
 			});
 
 
@@ -93,10 +140,95 @@ $(function(){
 
 	$('.masu').on('click',function(){
 		if($('.koma').hasClass('koma_on')){
-			Tmp_T=$(this).css('top');
-			Tmp_L=$(this).css('left');
-			$('.koma_on').animate({'left':Tmp_L,'top':Tmp_T},300);
+
+			Tmp		=$('.koma_on').attr('id').replace('koma','');
+			Tmp_cc	=$(this).attr('cc');
+			Tmp_ll	=$(this).attr('ll');
+
+			Tmp_C	=72-$(this).attr('cc')*8;
+			Tmp_L	=$(this).attr('ll') *9 -5 ;
+
+			$('.koma_on').animate({'left':Tmp_C+'vw','top':Tmp_L+'vw'},300);
 			$('.koma').removeClass('koma_on');
+
+			if($(this).hasClass('s0')){
+				if(	KB[Tmp] == 9 ||	KB[Tmp] == 8){
+
+					//■恐怖無条件成
+					if(Tmp_ll == 1 && Player ==0){
+						Tmp_ch=1;
+
+					}else if(Tmp_ll == 9 && Player ==1){
+						Tmp_ch=1;
+
+
+					//■恐怖成確認
+					}else if(Tmp_ll < 4 && Player ==0){
+						Tmp_ch=2;
+
+					}else if(Tmp_ll > 6 && Player ==1){
+						Tmp_ch=2;
+
+					}
+				} else if(KB[Tmp] == 7){
+					//■K無条件成
+					if(Tmp_ll < 3 && Player ==0){
+						Tmp_ch=1;
+
+					}else if(Tmp_ll >7 && Player ==1){
+						Tmp_ch=1;
+
+
+					//■K成確認
+					}else if(Tmp_ll == 3 && Player ==0){
+						Tmp_ch=2;
+
+					}else if(Tmp_ll == 7 && Player ==1){
+						Tmp_ch=2;
+
+					}
+
+				}else	if(	KB[Tmp] == 3 ||	KB[Tmp] == 4 ||	KB[Tmp] == 6){
+					if(Tmp_ll < 4 && Player ==0){
+						Tmp_ch=2;
+
+					}else if(Tmp_ll > 6 && Player ==1){
+						Tmp_ch=2;
+
+					}
+				}
+			}
+
+
+			Te_Count++;
+			$.post({
+				url:"./post/move_get.php",
+				data:{
+					'move_id'	:Tmp,
+					'move_ll'	:Tmp_ll,
+					'move_cc'	:Tmp_cc,
+					'move_ch'	:Tmp_ch,
+					'count'		:Te_Count,
+				},
+
+			}).done(function(data, textStatus, jqXHR){
+
+console.log("□"+Coma[Player]  + Tmp_cc+ Masu_L[Tmp_ll]);
+
+				TmpId	="";
+				Tmp2	="";
+				Tmp		="";
+				Tmp_cc	="";
+				Tmp_ll	="";
+				Tmp_ch	="";
+
+				Tmp_C	="";
+				Tmp_L	="";
+
+			}).fail(function(jqXHR, textStatus, errorThrown){
+				console.log(textStatus);
+				console.log(errorThrown);
+			});
 		}
 	});
 });
