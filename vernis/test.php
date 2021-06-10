a1c15e06701033a76d31aead17e1bd8315827b6c
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-06-10 09:28:15 +0900

    ar

--- vernis/test.php
+++ vernis/test.php
@@ -1,12 +1,135 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+<style>
+.back{
+	position:fixed;
+	top		:-10vh;
+	left	:-10vh;
+	width	:120vw;
+	height	:120vh;
+	margin	:auto;
+	z-index	:-1;
+}
+body{
+	text-align:center;
+}
+.box_a{
+	width:60vw;
+	margin:0 auto;
+	border:1px solid #303030;
+	background:linear-gradient(200,200,200,0.2);
+}
+</style>
 </head>
 <body>
-    
+<img src="./howto.png" class="back">    
+<div class="box_a">
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+</div>
+<br>
+<br>
+<br>
+<div class="box_a">
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+</div>
+<div class="box_a">
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+</div>
+<br>
+<br>
+<br>
+<div class="box_a">
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+</div>
+<div class="box_a">
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+</div>
+<br>
+<br>
+<br>
+<div class="box_a">
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+</div>
+<div class="box_a">
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+</div>
+<br>
+<br>
+<br>
+<div class="box_a">
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+あいうえお<br>
+</div>
+ 
 </body>
 </html>
+
+
