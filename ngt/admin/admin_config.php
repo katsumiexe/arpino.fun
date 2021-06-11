f88f1bc02bcb679c9287fb8b06ca3b8885e70fc8
katsumiexe <counterpost2016@gmail.com>
2021-06-11 20:06:35 +0900

    ar

--- ngt/admin/admin_config.php
+++ ngt/admin/admin_config.php
@@ -1,4 +1,5 @@
 <?
+
 $sql ="SELECT * FROM wp01_0check_main";
 $sql.=" ORDER BY sort ASC";
 if($result = mysqli_query($mysqli,$sql)){
@@ -151,41 +152,41 @@ $(function(){
 		<td class="config_sche_list">
 			<select name="set_time" class="set_box">
 			<option value="0">24時</option>
-			<option value="1" <?if($start_time="1"){?> selected="selected"<?}?>>01時</option>
-			<option value="2" <?if($start_time="2"){?> selected="selected"<?}?>>02時</option>
-			<option value="3" <?if($start_time="3"){?> selected="selected"<?}?>>03時</option>
-			<option value="4" <?if($start_time="4"){?> selected="selected"<?}?>>04時</option>
-			<option value="5" <?if($start_time="5"){?> selected="selected"<?}?>>05時</option>
-			<option value="6" <?if($start_time="6"){?> selected="selected"<?}?>>06時</option>
-			<option value="7" <?if($start_time="7"){?> selected="selected"<?}?>>07時</option>
-			<option value="8" <?if($start_time="8"){?> selected="selected"<?}?>>08時</option>
-			<option value="9" <?if($start_time="9"){?> selected="selected"<?}?>>09時</option>
-			<option value="10" <?if($start_time="10"){?> selected="selected"<?}?>>10時</option>
-			<option value="11" <?if($start_time="11"){?> selected="selected"<?}?>>11時</option>
-			<option value="12" <?if($start_time="12"){?> selected="selected"<?}?>>12時</option>
-			<option value="13" <?if($start_time="13"){?> selected="selected"<?}?>>13時</option>
-			<option value="14" <?if($start_time="14"){?> selected="selected"<?}?>>14時</option>
-			<option value="15" <?if($start_time="15"){?> selected="selected"<?}?>>15時</option>
-			<option value="16" <?if($start_time="16"){?> selected="selected"<?}?>>16時</option>
-			<option value="17" <?if($start_time="17"){?> selected="selected"<?}?>>17時</option>
-			<option value="18" <?if($start_time="18"){?> selected="selected"<?}?>>18時</option>
-			<option value="19" <?if($start_time="19"){?> selected="selected"<?}?>>19時</option>
-			<option value="20" <?if($start_time="20"){?> selected="selected"<?}?>>20時</option>
-			<option value="21" <?if($start_time="21"){?> selected="selected"<?}?>>21時</option>
-			<option value="22" <?if($start_time="22"){?> selected="selected"<?}?>>22時</option>
-			<option value="23" <?if($start_time="23"){?> selected="selected"<?}?>>23時</option>
+			<option value="1" <?if($start_time=="1"){?> selected="selected"<?}?>>01時</option>
+			<option value="2" <?if($start_time=="2"){?> selected="selected"<?}?>>02時</option>
+			<option value="3" <?if($start_time=="3"){?> selected="selected"<?}?>>03時</option>
+			<option value="4" <?if($start_time=="4"){?> selected="selected"<?}?>>04時</option>
+			<option value="5" <?if($start_time=v"5"){?> selected="selected"<?}?>>05時</option>
+			<option value="6" <?if($start_time=="6"){?> selected="selected"<?}?>>06時</option>
+			<option value="7" <?if($start_time=="7"){?> selected="selected"<?}?>>07時</option>
+			<option value="8" <?if($start_time=="8"){?> selected="selected"<?}?>>08時</option>
+			<option value="9" <?if($start_time=="9"){?> selected="selected"<?}?>>09時</option>
+			<option value="10" <?if($start_time=="10"){?> selected="selected"<?}?>>10時</option>
+			<option value="11" <?if($start_time=="11"){?> selected="selected"<?}?>>11時</option>
+			<option value="12" <?if($start_time=="12"){?> selected="selected"<?}?>>12時</option>
+			<option value="13" <?if($start_time=="13"){?> selected="selected"<?}?>>13時</option>
+			<option value="14" <?if($start_time=="14"){?> selected="selected"<?}?>>14時</option>
+			<option value="15" <?if($start_time=="15"){?> selected="selected"<?}?>>15時</option>
+			<option value="16" <?if($start_time=="16"){?> selected="selected"<?}?>>16時</option>
+			<option value="17" <?if($start_time=="17"){?> selected="selected"<?}?>>17時</option>
+			<option value="18" <?if($start_time=="18"){?> selected="selected"<?}?>>18時</option>
+			<option value="19" <?if($start_time=="19"){?> selected="selected"<?}?>>19時</option>
+			<option value="20" <?if($start_time=="20"){?> selected="selected"<?}?>>20時</option>
+			<option value="21" <?if($start_time=="21"){?> selected="selected"<?}?>>21時</option>
+			<option value="22" <?if($start_time=="22"){?> selected="selected"<?}?>>22時</option>
+			<option value="23" <?if($start_time=="23"){?> selected="selected"<?}?>>23時</option>
 			</select>
 		</td>
 		<td class="config_sche_top" style="width:120px;">開始曜日</td>
 		<td class="config_sche_list">
 			<select name="set_week" class="set_box">
 			<option value="0">日曜日</option>
-			<option value="1" <?if($start_week="1"){?> selected="selected"<?}?>>月曜日</option>
-			<option value="2" <?if($start_week="2"){?> selected="selected"<?}?>>火曜日</option>
-			<option value="3" <?if($start_week="3"){?> selected="selected"<?}?>>水曜日</option>
-			<option value="4" <?if($start_week="4"){?> selected="selected"<?}?>>木曜日</option>
-			<option value="5" <?if($start_week="5"){?> selected="selected"<?}?>>金曜日</option>
-			<option value="6" <?if($start_week="6"){?> selected="selected"<?}?>>土曜日</option>
+			<option value="1" <?if($start_week=="1"){?> selected="selected"<?}?>>月曜日</option>
+			<option value="2" <?if($start_week=="2"){?> selected="selected"<?}?>>火曜日</option>
+			<option value="3" <?if($start_week=="3"){?> selected="selected"<?}?>>水曜日</option>
+			<option value="4" <?if($start_week=="4"){?> selected="selected"<?}?>>木曜日</option>
+			<option value="5" <?if($start_week=="5"){?> selected="selected"<?}?>>金曜日</option>
+			<option value="6" <?if($start_week=="6"){?> selected="selected"<?}?>>土曜日</option>
 			</select>
 		</td>
 	</tr>
